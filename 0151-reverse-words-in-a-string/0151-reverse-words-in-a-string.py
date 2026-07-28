b250cb995877ccb47dd294ff40efa351ef30bbd0
Shamanth S R <120055385+shamanth-sr@users.noreply.github.com>
2026-07-28 23:19:14 +0530

    Time: 3 ms (32%), Space: 19.5 MB (10.61%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,24 +1,34 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # Initialise a list to store the word
-        words = []
 
-        word = ""
+# Solving in space: O(1), using result string
+               
+        result = ""
+        i = len(s) - 1
 
-        # Loop through each character in the string's'
-        for ch in s:
-            # If we don't have a space b/w
-            if ch != " ":
-                word += ch
-            # If we found a space
-            elif word:
-                words.append(word)
-                word = ""
+        # Loop string 's', character by characrter
+        while i >= 0:
+            #checking if character is an empty, then decrease by 1
+            while i>=0 and s[i]==" ":
+                i -= 1
 
-        # Last word
-        if word:
-            words.append(word)
+            # Out-of-bound/ Empty
+            if i < 0:
+                break
 
-        words.reverse()
+            # Looping non-emppty character, till a space is found
+            end = i
+            while i>=0 and s[i] != " ":
+                i -= 1
 
-        return " ".join(words)
+            word = s[i+1 : end+1]
+
+            # Add a space, if it is not an empty string
+            if result != "":
+                result += " " 
+
+            result += word
+
+        return result
+
+
