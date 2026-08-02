ab0cc4cdfe761b787ab19089b035e1ea74f9849a
Shamanth S R <120055385+shamanth-sr@users.noreply.github.com>
2026-08-02 23:22:12 +0530

    Time: 10 ms (79.39%), Space: 19.5 MB (46.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        S = sorted(s)
-        T = sorted(t)
+        if len(s) != len(t):
+            return False
 
-        return True if T == S else False
+        freq = [0] * 26
+
+        # Here the only said that only lowwercase letters are present
+        # So we'll set range a - z
+        
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+
+        for char in t:
+            freq[ord(char) - ord('a')] -= 1
+
+        for count in freq:
+            if count != 0:
+                return False
+        
+        return True
